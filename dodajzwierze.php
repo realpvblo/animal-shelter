057ed394506bde7cf056010cedf6b8cb18b6e1b0
realpvblo <78243008+realpvblo@users.noreply.github.com>
2023-06-01 00:12:03 +0200

    dodajzwierze

--- dodajzwierze.php
+++ dodajzwierze.php
@@ -10,6 +10,7 @@
       padding: 20px;
       border: 1px solid #ccc;
       border-radius: 5px;
+      margin-top: 5px;
     }
     .form-container label {
       display: block;
@@ -132,64 +133,67 @@
   }
   ?>
 
-  <div class="form-container">
-    <form method="POST" action="" enctype="multipart/form-data">
-      <label for="typ">Typ:</label>
-      <select name="typ" id="typSelect" required onchange="updateRasy()">
-        <option value="kot">Kot</option>
-        <option value="pies">Pies</option>
-      </select>
+  <div>
+    <h1 style="font-size: 30px; justify-content: center; display: flex; margin-top: 5%; margin-bottom: 2%; font-weight: bold;">Dodaj zwierzę do bazy danych</h1>
+    <div class="form-container">
+      <form method="POST" action="" enctype="multipart/form-data">
+        <label for="typ">Typ:</label>
+        <select name="typ" id="typSelect" required onchange="updateRasy()">
+          <option value="kot">Kot</option>
+          <option value="pies">Pies</option>
+        </select>
 
-      <label for="imie">Imię:</label>
-      <input type="text" name="imie" required>
+        <label for="imie">Imię:</label>
+        <input type="text" name="imie" required>
 
-      <label for="wiek">Wiek:</label>
-      <select name="wiek" required>
-        <?php
-        // Generowanie opcji dla wieku 1-20
-        for ($i = 1; $i <= 20; $i++) {
-          echo "<option value='$i'>$i</option>";
-        }
-        ?>
-      </select>
+        <label for="wiek">Wiek:</label>
+        <select name="wiek" required>
+          <?php
+          // Generowanie opcji dla wieku 1-20
+          for ($i = 1; $i <= 20; $i++) {
+            echo "<option value='$i'>$i</option>";
+          }
+          ?>
+        </select>
 
-      <label for="rasa">Rasa:</label>
-      <select name="rasa" id="rasaSelect" required></select>
+        <label for="rasa">Rasa:</label>
+        <select name="rasa" id="rasaSelect" required></select>
 
-      <label for="zdjecie">Zdjęcie (link):</label>
-      <input type="text" name="zdjecie" required>
+        <label for="zdjecie">Zdjęcie (link):</label>
+        <input type="text" name="zdjecie" required>
 
-      <label for="opis">Opis:</label>
-      <textarea name="opis" required></textarea>
+        <label for="opis">Opis:</label>
+        <textarea name="opis" required></textarea>
 
-      <input type="submit" name="submit" value="Dodaj zwierzę">
+        <input type="submit" name="submit" value="Dodaj zwierzę">
 
-      <script>
-        function updateRasy() {
-          var typSelect = document.getElementById("typSelect");
-          var rasaSelect = document.getElementById("rasaSelect");
-          rasaSelect.innerHTML = ""; // Wyczyść opcje pola rasy
+        <script>
+          function updateRasy() {
+            var typSelect = document.getElementById("typSelect");
+            var rasaSelect = document.getElementById("rasaSelect");
+            rasaSelect.innerHTML = ""; // Wyczyść opcje pola rasy
 
-          if (typSelect.value === "kot") {
-            var kotRasy = ['Dachowiec', 'Bezdomny', 'Inna'];
-            for (var i = 0; i < kotRasy.length; i++) {
-              var option = document.createElement("option");
-              option.text = kotRasy[i];
-              option.value = kotRasy[i];
-              rasaSelect.add(option);
-            }
-          } else if (typSelect.value === "pies") {
-            var piesRasy = ['Labrador', 'Husky', 'Inna'];
-            for (var i = 0; i < piesRasy.length; i++) {
-              var option = document.createElement("option");
-              option.text = piesRasy[i];
-              option.value = piesRasy[i];
-              rasaSelect.add(option);
+            if (typSelect.value === "kot") {
+              var kotRasy = ['Dachowiec', 'Bezdomny', 'Inna'];
+              for (var i = 0; i < kotRasy.length; i++) {
+                var option = document.createElement("option");
+                option.text = kotRasy[i];
+                option.value = kotRasy[i];
+                rasaSelect.add(option);
+              }
+            } else if (typSelect.value === "pies") {
+              var piesRasy = ['Labrador', 'Husky', 'Inna'];
+              for (var i = 0; i < piesRasy.length; i++) {
+                var option = document.createElement("option");
+                option.text = piesRasy[i];
+                option.value = piesRasy[i];
+                rasaSelect.add(option);
+              }
             }
           }
-        }
-      </script>
-    </form>
+        </script>
+      </form>
+    </div>
   </div>
 </body>
 </html>
